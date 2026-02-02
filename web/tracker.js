2bfe6924a01ce2ccc746ddb34f30f083e3c69951
Igor Kishchuk <161873324+KIS-IT-PRODUCTION@users.noreply.github.com>
2026-02-02 22:05:54 +0200

    Update tracker.js

--- web/tracker.js
+++ web/tracker.js
@@ -30,7 +30,7 @@ var Devizer = {
                 }
             }
         }
-        req.open('GET', 'https://product.devizer.pro/admin/', true);
+        req.open('GET', 'https://product.devizer.pro', true);
         req.setRequestHeader('DAPROPS', propertiesAsString);
         req.send(null);
     }
